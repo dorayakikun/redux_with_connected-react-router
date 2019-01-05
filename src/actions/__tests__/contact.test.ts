a9095e7d9d8a4b8c95f320e9e6a39c16bf50bd20
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-01-05 17:54:38 +0900

    chore: Add unit tests

---
+++ src/actions/__tests__/contact.test.ts
@@ -0,0 +1,29 @@
+import * as actions from "../contact";
+
+describe('actions', () => {
+  it('should create an action to set a contact form', () => {
+    const contactForm = {
+      firstName: "Kiyokado",
+      lastName: "Komatsu",
+      email: undefined
+    }
+    const expectedAction = {
+      type: actions.ActionKeys.SET_CONTACT_FORM,
+      payload: {...contactForm}
+    }
+    expect(actions.setContactForm(contactForm)).toEqual(expectedAction)
+  });
+
+  it('should create an action to submit a contact form', () => {
+    const contactForm = {
+      firstName: "Kiyokado",
+      lastName: "Komatsu",
+      email: undefined
+    }
+    const expectedAction = {
+      type: actions.ActionKeys.SUBMIT_CONTACT_FORM,
+      payload: {...contactForm}
+    }
+    expect(actions.submitContactForm(contactForm)).toEqual(expectedAction)
+  });
+});
