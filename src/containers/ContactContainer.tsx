0e69a260ab304e552a5b1d1c82892de87ce4b271
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-02-21 21:51:31 +0900

    chore: Add CI Settings (#6)

--- src/containers/ContactContainer.tsx
+++ src/containers/ContactContainer.tsx
@@ -1,16 +1,11 @@
 import * as React from "react";
 import { Dispatch } from "redux";
-import { InjectedFormProps } from "redux-form";
 
 import { submitContactForm } from "../actions/contact";
 import { ContactForm, ContactFormFields } from "../components/ContactForm";
 
 export class ContactContainer extends React.Component {
-  submit = (
-    values: ContactFormFields,
-    dispatch: Dispatch<any>,
-    props: InjectedFormProps<ContactFormFields>
-  ) => {
+  submit = (values: ContactFormFields, dispatch: Dispatch<any>) => {
     dispatch(submitContactForm(values));
   };
   render() {
